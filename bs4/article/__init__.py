8da504a71c6e237bb9e5b83e86ec38a09317ef05
xubinbin1024 <596928539@qq.com>
2017-08-17 13:21:15 +0800

    find_all

--- bs4/article/__init__.py
+++ bs4/article/__init__.py
@@ -24,23 +24,30 @@ html_doc = """
 """
 
 soup = BeautifulSoup(html_doc, "html.parser")
+# -------------------------------------------------------------------------------------
 # print soup.prettify()# 格式化输出 HTML文档
 # print soup.title
 # print soup.title.name
 # print soup.title.string
 # print soup.p
 
-
+# -------------------------------------------------------------------------------------
 # 查找 p 元素，且 该 p 元素满足 class属性的值为 story
 # p_class.text 、p_class.string、p_class.get_text() 等价
+
 # for p_class in soup.find_all('p', {'class': 'story'}):
 #     print p_class.get_text()
+
+# -------------------------------------------------------------------------------------
+
 # for p_class in soup.find_all('p', {'class': re.compile('^s[A-Z].*s$')}):
 #     print p_class.get_text()
 
+# -------------------------------------------------------------------------------------
 # 打印出第一个 a 标签
-# print soup.a
 
+# print soup.a
+# -------------------------------------------------------------------------------------
 # 打印出所有的 a 标签
 # i = 0
 # for link in soup.find_all('a'):
@@ -48,7 +55,11 @@ soup = BeautifulSoup(html_doc, "html.parser")
 #     print '第 ', i, ' 个 a 标签的链接 = ', link[
 #         'href'], '标签的内容 = ', link.string
 
-i = 0
-for link in soup.find_all('a'):
-    i = i + 1
-    print  type(link),link['href'], link.string, link['id'], len(link)
+# -------------------------------------------------------------------------------------
+# i = 0
+# for link in soup.find_all(name='a'):
+#     i = i + 1
+#     print  type(link), link['href'], link.string, link['id'], len(link)
+# -------------------------------------------------------------------------------------
+# 从文档中获取所有文本内容
+print soup.get_text()
